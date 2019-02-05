d76bfc544f3fa7134acdf9f5b6070bfbc4c7e410
Ruben <33250407+Ruben3211@users.noreply.github.com>
2019-02-05 07:20:40 +0100

    Update dbconnect.php

--- PHP/dbconnect.php
+++ PHP/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 // 1. Connectie leggen met de database
 $dbhost = "localhost";
-$dbuser = "onera";
-$dbpass = "Frankarm";
+$dbuser = "root";
+$dbpass = "";
 $dbname = "onera";
 $db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
  // Test of de verbinding werkt!
